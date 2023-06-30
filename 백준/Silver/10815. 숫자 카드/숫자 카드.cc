2d7d4cfe019012fdda72b736833c3b0a17da9c45
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-06-30 19:05:33 +0900

    [Silver V] Title: 숫자 카드, Time: 272 ms, Memory: 5220 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> card;
+int n;
+
+int binarySearch(int find) {
+    int l = 0, r = n;
+    while (l < r)
+    {
+        int mid = (l+r) / 2;
+        if (card[mid] == find) {
+            return 1;
+        } else if (card[mid] < find) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
+
+    }
+    return 0;
+}
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        int temp;
+        cin >> temp;
+        card.push_back(temp);
+    }
+    sort(card.begin(), card.end());
+
+    int m, k;
+    cin >> m;
+
+    for (int i = 0; i < m; i++){
+        cin >> k;
+        cout << binarySearch(k) << ' ';
+    }
+    return 0;
+}
