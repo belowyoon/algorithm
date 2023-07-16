e12a0c627a230ef096583ce9f880d13ce1c8b340
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-07-17 03:27:59 +0900

    [Silver III] Title: N과 M (7), Time: 480 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> selected;
+vector<int> nums;
+int n, m;
+
+void permutation(int depth) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            cout << selected[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        selected[depth] = nums[i];
+        permutation(depth+1);
+    }
+}
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+
+    cin >> n >> m;
+    int t;
+
+    for (int i = 0; i < n; i++) {
+        cin >> t;
+        nums.push_back(t);
+    }
+    sort(nums.begin(), nums.end());
+    selected.assign(m,0);
+
+    permutation(0);
+    return 0;
+}
