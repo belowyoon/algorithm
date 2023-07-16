24b5726b8b7e615e53fba1cc03bfdd33fa48ab4a
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-07-17 03:13:13 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<int> selected;
+int n, m;
+
+void combination(int depth, int nxt) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            cout << selected[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for (int i = nxt; i <= n; i++) {
+        selected[depth] = i;
+        combination(depth+1, i);
+    }
+}
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+
+    cin >> n >> m;
+    selected.assign(m,0);
+    combination(0,1);
+    return 0;
+}
