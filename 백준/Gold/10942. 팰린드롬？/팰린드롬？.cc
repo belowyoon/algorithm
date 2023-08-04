2a01d847aa6e29f5b53e15f44e309975ef4f75ad
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-08-04 20:26:59 +0900

    [Gold IV] Title: 팰린드롬?, Time: 272 ms, Memory: 17864 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -29,26 +29,17 @@ int main()
     for (int i = 1; i < n; i++) {
         // i는 j - j+i까지 보는 역할
         for (int j = 0; j < n - i; j++) {
-            //cout << j << " " << j+i << '\n';
             if (num[j] == num[j+i] && dp[j+1][j+i-1] == 1) {
                 dp[j][j+i] = 1;
             } 
         }
     }
-
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0;  j < n; j++) {
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << '\n';
-    // }
-
     int t, x, y;
     cin >> t;
     for (int i = 0; i < t; i++) {
         cin >> x >> y;
         cout << dp[x-1][y-1] << '\n';
     }
-
+    
     return 0;
 }
