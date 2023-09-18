b0adc6c9ef2100c3855a84019b90d778283b6ae6
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-09-18 15:22:30 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+int num[11];
+int maxNum = -1000000001;
+int minNum = 1000000001;
+
+void cal(int k, int oper[], int result) {
+    if (k == 0) {
+        maxNum = max(maxNum, result);
+        minNum = min(minNum, result);
+        return;
+    }
+    for (int j = 0; j < 4; j++) {
+        int temp = oper[j];
+        while (oper[j] > 0) {
+            oper[j]--;
+            switch(j) {
+                case 0:
+                    cal(k - 1, oper, result + num[n - k]);
+                    break;
+                case 1:
+                    cal(k - 1, oper, result - num[n - k]);
+                    break;
+                case 2:
+                    cal(k - 1, oper, result * num[n - k]);
+                    break;
+                case 3:
+                    cal(k - 1, oper, result / num[n - k]);
+                    break; 
+            } 
+        }    
+        oper[j] = temp;      
+    }
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+
+    cin >> n;
+    int oper[4] = {0};
+
+    for (int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    for (int i = 0; i < 4; i++) {
+        cin >> oper[i];
+    }
+
+    cal(n-1, oper, num[0]);
+    cout << maxNum << '\n' << minNum;
+
+    return 0;
+}
