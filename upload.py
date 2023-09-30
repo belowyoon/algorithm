a937c3ffaff1cf7118b741573faca4b184013a81
이하윤 <92996412+hayoon0524@users.noreply.github.com>
2023-10-01 02:53:30 +0900

    Update upload.py

--- upload.py
+++ upload.py
@@ -6,48 +6,53 @@ HEADER="""#
 """
 
 def main():
-  content = ""
-  content += HEADER
-  
-  directories = []
-  solveds = []
-  
-  for root, dirs, files in os.walk("."):
-    dirs.sort()
-    if root == '.':
-      for dir in ('.git', '.github'):
-        try:
-          dirs.remove(dir)
-        except ValueError:
-          pass
-      continue
-    
-    category = os.path.basename(root)
-    
-    if category == 'images':
-      continue
-      
-    directory = os.path.basename(os.path.dirname(root))
-    
-    if directory == '.':
-      continue
-      
-    if directory not in directories:
-      if directory in ["백준", "프로그래머스"]:
-        content += "## 📚 {}\n".format(directory)
-      else:
-        content += "### 🚀 {}\n".format(directory)
-        content += "| 문제번호 | 링크 |\n"
-        content += "| ----- | ----- |\n"
-      directories.append(directory)
-      
-    for file in files:
-      if category not in solveds:
-        content += "|{}|[링크]({})|\n".format(category, parse.quote(os.path.join(root, file)))
-        solveds.append(category)
-        
-  with open("README.md", "w") as fd:
-    fd.write(content)
-    
+    content = ""
+    content += HEADER
+
+    categories = {
+        "Gold": [],
+        "Silver": [],
+        "Bronze": [],
+        "Other": []
+    }
+
+    for root, dirs, files in os.walk("."):
+        dirs.sort()
+        if root == '.':
+            for dir in ('.git', '.github'):
+                try:
+                    dirs.remove(dir)
+                except ValueError:
+                    pass
+            continue
+
+        category = os.path.basename(root)
+        if category in ["images", ".git", ".github"]:
+            continue
+
+        directory = os.path.basename(os.path.dirname(root))
+        if directory not in ["백준", "프로그래머스"]:
+            if category.startswith("Gold"):
+                categories["Gold"].append(category)
+            elif category.startswith("Silver"):
+                categories["Silver"].append(category)
+            elif category.startswith("Bronze"):
+                categories["Bronze"].append(category)
+            else:
+                categories["Other"].append(category)
+
+    for medal, medal_categories in categories.items():
+        if medal_categories:
+            content += "## {} 🏅\n".format(medal)
+            content += "| 문제번호 | 링크 |\n"
+            content += "| ----- | ----- |\n"
+            for category in medal_categories:
+                for file in os.listdir(os.path.join(category)):
+                    content += "|{}|[링크]({})|\n".format(file, parse.quote(os.path.join(category, file)))
+                content += "\n"
+
+    with open("README.md", "w") as fd:
+        fd.write(content)
+
 if __name__ == "__main__":
-  main()
+    main()
