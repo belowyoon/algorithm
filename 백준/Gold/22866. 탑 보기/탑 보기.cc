5406af0e999d64c0fb4dd53bd3ce9580071f31b4
belowyoon <92996412+belowyoon@users.noreply.github.com>
2024-04-26 02:07:51 +0900

    [Gold III] Title: 탑 보기, Time: 32 ms, Memory: 4120 KB -BaekjoonHub

---
+++ 백준/Gold/22866. 탑 보기/탑 보기.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    
+    vector<int> arr(n+1, 0);
+    vector<int> size(n+1, 0);
+    vector<pair<int, int>> dist(n+1, {-1, -1});
+    for(int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    stack <int> l;
+    stack <int> r;
+
+    for (int i = n-1; i >= 1; i--) {
+        if (arr[i+1] > arr[i]) {
+            while (!r.empty() && arr[r.top()] <= arr[i+1]) {
+                r.pop();
+            }
+            r.push(i+1);
+        } else {
+            while (!r.empty() && arr[r.top()] <= arr[i]) {
+                r.pop();
+            }
+        }
+        size[i] += r.size();
+        if (!r.empty()) dist[i].second = r.top();
+    }
+    for (int i = 2; i <= n; i++) {
+        if (arr[i-1] < arr[i]) {
+            while (!l.empty() && arr[l.top()] <= arr[i]) {
+                l.pop();
+            }
+        } else if (arr[i-1] > arr[i]) {
+            while (!l.empty() && arr[l.top()] <= arr[i-1]) {
+                l.pop();
+            }
+            l.push(i-1);
+        } 
+        size[i] += l.size();
+        if (!l.empty()) dist[i].first = l.top();
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << size[i] << " ";
+        if (size[i] > 0) {
+            if (dist[i].first == -1) {
+                cout << dist[i].second;
+            } else if (dist[i].second == -1) {
+                cout << dist[i].first;
+            } else {
+                if ((i - dist[i].first) <= (dist[i].second - i)) {
+                    cout << dist[i].first;
+                } else {
+                    cout << dist[i].second;
+                }
+            }
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
